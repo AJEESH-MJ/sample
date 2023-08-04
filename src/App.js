9f1c2fcd1fdbc2151bcb9e27c2bcc2eb19fb32ec
AJEESH-MJ <ajeeshjayanm@gmail.com>
2023-08-04 10:43:36 +0530

    Routes upfdated

--- src/App.js
+++ src/App.js
@@ -18,13 +18,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/products" element={<Products />} />
-          <Route exact path="/design" element={<Design />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/Auth" element={<Auth />} />
-          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="Y_BRO/" element={<Home />} />
+          <Route exact path="Y_BRO/products" element={<Products />} />
+          <Route exact path="Y_BRO/design" element={<Design />} />
+          <Route exact path="Y_BRO/about" element={<About />} />
+          <Route exact path="Y_BRO/contact" element={<Contact />} />
+          <Route exact path="Y_BRO/Auth" element={<Auth />} />
+          <Route exact path="Y_BRO/cart" element={<Cart />} />
         </Routes>
         <Footer />
       </Router>
